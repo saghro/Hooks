22e30b27782f35ac019324976710c9970ef75587
Saghroayoub <ayoubsaghro27@gmail.com>
2023-03-25 03:01:38 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,61 @@
+import img1 from './images/img1.jpeg';
+import img2 from './images/img2.jpeg';
+import imag3 from './images/imag3.jpeg';
+import img4 from './images/img4.jpeg';
+import img5 from './images/img5.jpeg';
+import img6 from './images/img6.jpeg';
+import img7 from './images/img7.jpeg';
+const data ={
+    cardData:[
+        {
+            id:1,
+            img:img1,
+            title:'gdyazdg',
+            desc:'jkdgk'
+           
+        }
+        ,
+        {
+            id:1,
+            img:img2,
+            title:'gdyazdg',
+            desc:'jfiugk'
+        }
+        ,
+         {
+            id:1,
+            img:imag3,
+            title:'gdyaz^ldkaozjdg',
+            desc:'bshd'
+        }
+        , 
+        {
+            id:1,
+            img:img4,
+            title:'ndizah',
+            desc:'bshdjdz'
+        }
+        ,
+        {
+            id:1,
+            img:img5,
+            title:'gdyfyufazdg',
+            desc:'bshyiiyd'
+        }
+        ,
+        {
+            id:1,
+            img:img6,
+            title:'gdyazdg',
+            desc:'bshd'
+        }
+        ,
+        {
+            id:1,
+            img:img7,
+            title:'gdyazdg',
+            desc:'bshd'
+        }
+    ]
+}
+export default data
